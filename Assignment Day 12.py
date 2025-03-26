74416b1077384e38914d00c3f5c4927b898f1fac
Sowmi0901 <sowmiyasatheesh308@gmail.com>
2025-03-26 19:18:01 +0530

    Add files via upload

---
+++ Assignment Day 12.py
@@ -0,0 +1,8 @@
+temperature = float(input("what is the temperature in celcius"))
+if temperature < 10:
+             print("It's cold! Wear a warm coat and scarf.")
+elif temperature <= 25:
+            print("It's mild! A light jacket will do.")
+else:
+    print("It's hot! T-shirts would be perfect.")
+                
